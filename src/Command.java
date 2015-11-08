997dc18c67521c2e9b4735aa088a392f74a906bc
mgarna <r.garniy@yandex.ua>
2015-11-08 12:51:16 +0200

    new command

--- src/Command.java
+++ src/Command.java
@@ -3,20 +3,46 @@
  */
 
 public class Command {
-    private connector connect = new connector(" ",0);
 
-    public Command() {
+    private CommandType ChatCommand;
+
+    public Command(CommandType commandType) {
+        this.ChatCommand = commandType;
     }
 
-    public void disconnect() {
-        connect.disconnect();
+
+    @Override
+    public String toString() {
+        return "Command{" +
+                "ChatCommand=" + ChatCommand +
+                '}';
     }
 
-    public void sendMessage(String message) {
-        connect.sendMessage();
-        connect.sendMessage();
+    public static enum CommandType {
+        ACCEPT, DISCONNECT, MESSAGE, NICK, REJECT;
+
+        public static boolean trueCommand(String test) {
+
+            for (CommandType c : CommandType.values()) {
+                if (c.name().equals(test)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
 
+
+    public static Command getCommand(String string) {
+        if (string.contains("Accepted")) return new Command(CommandType.ACCEPT);
+        if (string.contains("Disconnect")) return new Command(CommandType.DISCONNECT);
+        if (string.contains("Message")) return new Command(CommandType.MESSAGE);
+        if (string.contains("Reject")) return new Command(CommandType.REJECT);
+
+
+        return null;
+    }
 
 
 }
